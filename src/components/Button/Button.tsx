9400a7823adf7620fda8e6c2724869220ea278cb
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-12-13 22:15:26 -0500

    Allow specifying just content for tooltip

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import { Tooltip, TooltipProps } from 'components/Tooltip';
-import { useMemo } from 'react';
+import { ReactNode, useMemo } from 'react';
 import styles from './Button.module.scss';
 
 export interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
-  tooltip?: TooltipProps;
+  tooltip?: ReactNode | TooltipProps;
 }
 
 export const Button = (props: ButtonProps) => {
@@ -20,5 +20,25 @@ export const Button = (props: ButtonProps) => {
     [onClick, children]
   );
 
-  return tooltip ? <Tooltip {...tooltip}>{button}</Tooltip> : button;
+  const tooltipProps = useMemo(() => {
+    if (tooltip === undefined) {
+      return undefined;
+    } else if (isTooltipProps(tooltip)) {
+      return tooltip;
+    } else {
+      return { children: tooltip };
+    }
+  }, [tooltip]);
+
+  return tooltip === undefined ? (
+    button
+  ) : (
+    <Tooltip {...tooltipProps}>{button}</Tooltip>
+  );
 };
+
+function isTooltipProps(
+  tooltip: ReactNode | TooltipProps
+): tooltip is TooltipProps {
+  return (tooltip as TooltipProps).children !== undefined;
+}
