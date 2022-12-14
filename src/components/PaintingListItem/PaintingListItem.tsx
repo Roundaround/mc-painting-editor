84d9215d06ba0e0710a8ffabbe4e4e993ac8b112
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-12-13 22:12:08 -0500

    Minor tweak to painting list

--- src/components/PaintingListItem/PaintingListItem.tsx
+++ src/components/PaintingListItem/PaintingListItem.tsx
@@ -78,69 +78,57 @@ export function PaintingListItem(props: PaintingListItemProps) {
           style={{
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'space-between',
-            height: '100%',
+            gap: 'var(--size-1)',
           }}
         >
-          <div>
-            ID: <pre>{painting.id}</pre>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 'var(--size-1)',
+          <TextInput
+            id={`painting-id-${painting.id}`}
+            label="ID"
+            value={painting.id}
+            onChange={(e) => {
+              updatePainting({ id: e.target.value });
             }}
-          >
-            <TextInput
-              id={`painting-id-${painting.id}`}
-              label="ID"
-              value={painting.id}
-              onChange={(e) => {
-                updatePainting({ id: e.target.value });
-              }}
-            />
-            <TextInput
-              id={`painting-name-${painting.id}`}
-              label="Name"
-              value={painting.name || ''}
-              onChange={(e) => {
-                updatePainting({ name: e.target.value });
-              }}
-            />
-            <TextInput
-              id={`painting-artist-${painting.id}`}
-              label="Artist"
-              value={painting.artist || ''}
-              onChange={(e) => {
-                updatePainting({ artist: e.target.value });
-              }}
-            />
-            <NumberInput
-              id={`painting-width-${painting.id}`}
-              label="Width"
-              min={1}
-              max={8}
-              value={painting.width}
-              onChange={(e) => {
-                updatePainting({
-                  width: parseInt(e.target.value, 10),
-                });
-              }}
-            />
-            <NumberInput
-              id={`painting-height-${painting.id}`}
-              label="Height"
-              min={1}
-              max={8}
-              value={painting.height}
-              onChange={(e) => {
-                updatePainting({
-                  height: parseInt(e.target.value, 10),
-                });
-              }}
-            />
-          </div>
+          />
+          <TextInput
+            id={`painting-name-${painting.id}`}
+            label="Name"
+            value={painting.name || ''}
+            onChange={(e) => {
+              updatePainting({ name: e.target.value });
+            }}
+          />
+          <TextInput
+            id={`painting-artist-${painting.id}`}
+            label="Artist"
+            value={painting.artist || ''}
+            onChange={(e) => {
+              updatePainting({ artist: e.target.value });
+            }}
+          />
+          <NumberInput
+            id={`painting-width-${painting.id}`}
+            label="Width"
+            min={1}
+            max={8}
+            value={painting.width}
+            onChange={(e) => {
+              updatePainting({
+                width: parseInt(e.target.value, 10),
+              });
+            }}
+          />
+          <NumberInput
+            id={`painting-height-${painting.id}`}
+            label="Height"
+            min={1}
+            max={8}
+            value={painting.height}
+            onChange={(e) => {
+              updatePainting({
+                height: parseInt(e.target.value, 10),
+              });
+            }}
+          />
         </div>
         <PaintingGrid
           maxHeight={8}
