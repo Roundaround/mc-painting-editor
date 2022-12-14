39aa2d9f4eb614c467f6e70ff206c0bbaa1de402
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-12-14 00:23:51 -0500

    Force dark theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="dark-theme">
       <Head />
       <body>
         <Main />
