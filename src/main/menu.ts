fc5c35544272e9f2231c474afae1c83c4d97991c
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-12-28 13:55:19 -0500

    Add development mode menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,4 +1,4 @@
-import { dialog, Menu, MenuItemConstructorOptions } from 'electron';
+import { Menu, MenuItemConstructorOptions } from 'electron';
 import { openZipFile, saveZipFile } from './files';
 import { store } from './store';
 
@@ -68,7 +68,66 @@ export const menuTemplate: MenuItemConstructorOptions[] = [
   },
 ];
 
+const developmentAdditions: MenuItemConstructorOptions[] = [
+  {
+    label: '&View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.reload();
+          }
+        },
+      },
+      {
+        label: 'Force Reload',
+        accelerator: 'CmdOrCtrl+Shift+R',
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.webContents.reloadIgnoringCache();
+          }
+        },
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Alt+Command+I';
+          } else {
+            return 'Ctrl+Shift+I';
+          }
+        })(),
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools();
+          }
+        },
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Ctrl+Command+F';
+          } else {
+            return 'F11';
+          }
+        })(),
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+      },
+    ],
+  },
+];
+
 export const registerMenu = () => {
-  const menu = Menu.buildFromTemplate(menuTemplate);
+  const menu = Menu.buildFromTemplate([
+    ...menuTemplate,
+    ...(process.env.NODE_ENV === 'development' ? developmentAdditions : []),
+  ]);
   Menu.setApplicationMenu(menu);
 };
