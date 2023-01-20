ed636d298fdaf89fdcad6ff906f6257de6d27979
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-01-19 21:14:59 -0500

    Add GitHub publisher to forge config

--- forge.config.ts
+++ forge.config.ts
@@ -1,9 +1,10 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import { PublisherGithub } from '@electron-forge/publisher-github';
+import type { ForgeConfig } from '@electron-forge/shared-types';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -16,6 +17,16 @@ const config: ForgeConfig = {
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
+  ],
+  publishers: [
+    new PublisherGithub({
+      repository: {
+        owner: 'roundaround',
+        name: 'mc-painting-editor',
+      },
+      draft: true,
+      prerelease: true,
+    }),
   ],
   plugins: [
     new WebpackPlugin({
