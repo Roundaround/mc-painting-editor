0c5733e4c8c161531b758fa5e9e1ba59cdd3f81b
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-01-02 01:46:46 -0500

    Get rid of before: tailwind prefixess in favor of an explicit overlay div

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -17,30 +17,48 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: ButtonVariant;
 };
 
-const InnerButton: FC<ButtonProps> = ({ variant, className, ...props }) => {
+const InnerButton: FC<ButtonProps> = ({
+  variant,
+  className,
+  children,
+  ...props
+}) => {
   return (
     <button
       className={clsxm(
-        'inline-block items-center justify-center rounded-md bg-blue-600 px-2 py-1 text-base font-medium text-gray-100 transition-[shadow,transform] duration-75 before:pointer-events-none before:absolute before:left-0 before:top-0 before:h-full before:w-full before:rounded-md before:bg-neutral-200 before:opacity-0 before:transition-opacity hover:scale-105 hover:shadow-lg hover:before:opacity-10 active:scale-95 active:before:opacity-0',
+        'group inline-block items-center justify-center rounded-md bg-blue-600 px-2 py-1 text-base font-medium text-gray-100 transition-[shadow,transform] duration-75 hover:scale-105 hover:shadow-lg active:scale-95',
         {
-          'rounded-2xl px-8 py-5 text-2xl font-bold before:rounded-2xl':
+          'rounded-2xl px-8 py-5 text-2xl font-bold':
             variant === ButtonVariant.LARGE,
           'inline-flex aspect-square p-0 [&>svg]:w-full':
             variant === ButtonVariant.ICON ||
             variant === ButtonVariant.ICON_MINI ||
             variant === ButtonVariant.ICON_TINY,
-          'w-8 rounded-full before:rounded-full':
-            variant === ButtonVariant.ICON,
-          'w-6 rounded-full before:rounded-full':
-            variant === ButtonVariant.ICON_MINI,
-          'w-4 rounded-full text-xs before:rounded-full':
-            variant === ButtonVariant.ICON_TINY,
+          'w-8 rounded-full': variant === ButtonVariant.ICON,
+          'w-6 rounded-full': variant === ButtonVariant.ICON_MINI,
+          'w-4 rounded-full text-xs': variant === ButtonVariant.ICON_TINY,
         },
         className,
       )}
       type="button"
       {...props}
-    />
+    >
+      <>
+        <div
+          className={clsxm(
+            'pointer-events-none absolute left-0 top-0 z-10 h-full w-full rounded-md bg-neutral-200 opacity-0 transition-opacity group-hover:opacity-10 group-active:opacity-0',
+            {
+              'rounded-2xl': variant === ButtonVariant.LARGE,
+              'rounded-full':
+                variant === ButtonVariant.ICON ||
+                variant === ButtonVariant.ICON_MINI ||
+                variant === ButtonVariant.ICON_TINY,
+            },
+          )}
+        />
+        {children}
+      </>
+    </button>
   );
 };
 
